2b65649fe5ca9cd1b4d92633bbd82d0b423f8a54
Igomigo <igomigofatai@gmail.com>
2024-09-03 08:55:24 +0100

    Email updates included

--- server_side/controllers/updateUserDetails.js
+++ server_side/controllers/updateUserDetails.js
@@ -5,30 +5,31 @@ async function updateUserDetails(request,response){
     try {
         const token = request.cookies.token || ""
 
-        const user = await getUserDetailsFromToken(token)
+        const user = await getUserDetailsFromToken(token);
 
-        const { name, profile_pic } = request.body
+        const { name, profile_pic, email } = request.body;
 
         const updateUser = await UserModel.updateOne({ _id : user._id },{
             name,
-            profile_pic
-        })
+            profile_pic,
+            email
+        });
 
-        const userInfomation = await UserModel.findById(user._id)
+        const userInfomation = await UserModel.findById(user._id);
 
         return response.json({
             message : "user data updated successfully",
             data : userInfomation,
             success : true
-        })
+        });
 
 
     } catch (error) {
         return response.status(500).json({
             message : error.message || error,
             error : true
-        })
+        });
     }
 }
 
-module.exports = updateUserDetails
+module.exports = updateUserDetails;
