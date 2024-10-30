ce4e772e676037ab220c70669cf670a916eae0a6
Igomigo <igomigofatai@gmail.com>
2024-10-30 17:15:28 +0100

    Update: Jwt expiry updated

--- server_side/controllers/checkPassword.js
+++ server_side/controllers/checkPassword.js
@@ -21,7 +21,7 @@ async function checkPassword(request,response){
             id : user._id,
             email : user.email 
         }
-        const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, { expiresIn : '1d'})
+        const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, { expiresIn : '14d'})
 
         const cookieOptions = {
             httpOnly : true,
